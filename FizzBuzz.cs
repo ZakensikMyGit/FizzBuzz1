0ace2468f1cc2691d084243a724ba15d6d1d97d9
ArturZakens <zakensik@gmail.com>
2023-05-03 00:48:16 +0200

    first commit

---
+++ FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string GetFizzBuzz(int number)
+        {
+            string text;
+            if (number % 3 == 0)
+            {
+                text = "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                text = "Buzz";
+            }
+            else if (number % 15 == 0)
+            {
+                text = "FizzBuzz";
+            }
+            else
+            {
+                text = $"Twoja liczba to {number}, GAME OVER";
+            }
+
+            return text;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<><><>\tZagrajmy w FizzBuzz\t<><><>\n******\t    Podaj liczbę:\t******\n");
+            int number = GetNumber();
+
+            var fizzBuzz = new FizzBuzz();
+            Console.WriteLine(fizzBuzz.GetFizzBuzz(number));
+
+            
+
+            Console.ReadLine();
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int result))
+                {
+                    Console.WriteLine("Podaj liczbę");
+                    continue;
+                }
+                return result;
+            }
+        }
+    }
+}
